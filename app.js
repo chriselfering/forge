dd498591da9a0e2a74923b379c5fd9aff3444c21
chriselfering <chriselfering@gmail.com>
2016-07-25 16:40:30 -0600

    shading on boxes

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+$( ".box" ).click(function() {
+    if (  $( this ).css( "transform" ) == 'none' ){
+        $(this).css("transform","rotate(10deg)");
+    } else {
+        $(this).css("transform","" );
+    }
+});
+
 
 
 
@@ -7,7 +15,32 @@ angular.module('App')
   .controller('MainCtlr', mainCtlr)
     function mainCtlr() {
       console.log('Main Controller reporting for duty!')
+
+      var MainCtlr = this;
+
+      this.accent= {}
+
+      MainCtlr.setAccent = function () {
+        this.accent = {
+          'border-radius': '20px',
+          'position': 'relative',
+          'transition-property': 'background-color, color',
+          'transition-duration': '1s',
+          'transition-timing-function': 'ease-out',
+          'background-color': '#1f0971',
+          'color':'black',
+          'box-shadow': '2px 2px 1px black'
+        }
+      };
+
+        // this.navbar = function () {
+        //   if (screen.width < 778) {
+        //     'background-color': '#1f0971'
+        //   }
+        // }
+
     };
+
 
 angular.module('App')
   .controller('StrCtrl', strCtrl)
