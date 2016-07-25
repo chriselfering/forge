ee0931610cc48f1bf0e481ad5e6fc7b4e9fa3f0e
chriselfering <chriselfering@gmail.com>
2016-07-25 14:37:32 -0600

    back to normal buttons

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-angular.module('App', [])
+
+
+
+angular.module('App', []);
 
 angular.module('App')
-  .controller(MainCtlr,
-    function() {
-      
-    })
+  .controller('MainCtlr', mainCtlr)
+    function mainCtlr() {
+      console.log('Main Controller reporting for duty!')
+    };
+
+angular.module('App')
+  .controller('StrCtrl', strCtrl)
+    function strCtrl() {
+      console.log('Strength Controller reporting for duty!')
+    }
+
+angular.module('App')
+  .controller('SpdCtrl', spdCtrl)
+    function spdCtrl() {
+      console.log('Speed Controller reporting for duty!')
+    }
+
+angular.module('App')
+  .controller('FinCtrl', finCtrl)
+    function finCtrl() {
+      console.log('Financial Controller reporting for duty!')
+    }
+
+
+    // bootstrap datepicker
+    // $('.datepicker').datepicker();
