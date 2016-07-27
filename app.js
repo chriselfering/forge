0dde267b67b4828b7e2bed1dabe99e8f92ac89c4
chriselfering <chriselfering@gmail.com>
2016-07-27 17:59:01 -0600

    last push of the day?

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ function ChartCtrl (StrFactory) {
       var innerWidth  = outerWidth  - margin.left - margin.right;
       var innerHeight = outerHeight - margin.top  - margin.bottom;
 
-      var svg = d3.select(".chart1").append("svg")
+      var svg = d3.select(".strChart").append("svg")
         .attr("width",  outerWidth)
         .attr("height", outerHeight);
       var g = svg.append("g")
@@ -120,73 +120,64 @@ function ChartCtrl (StrFactory) {
 // Chart 2
 // =======================================================================
 
-     var data = [
-     { name        : "Rainy Day Fund",
-       amount  : "158"},
-     { name        : "Retirement",
-       amount  : "245"},
-     { name        : "Car",
-       amount  : "185"},
-     { name        : "House",
-       amount  : "134"}
-     ]
+var data = StrFactory.goalArray;
 
-     var outerWidth = 500;
-     var outerHeight = 250;
-     var margin = { left: 200, top: 0, right: 0, bottom: 30 };
-     var barPadding = 0.2;
+var outerWidth = 500;
+var outerHeight = 250;
+var margin = { left: 200, top: 0, right: 0, bottom: 30 };
+var barPadding = 0.2;
 
-     var xColumn = "amount";
-     var yColumn = "name";
+var xColumn = "current";
+var yColumn = "name";
 
-     var innerWidth  = outerWidth  - margin.left - margin.right;
-     var innerHeight = outerHeight - margin.top  - margin.bottom;
+var innerWidth  = outerWidth  - margin.left - margin.right;
+var innerHeight = outerHeight - margin.top  - margin.bottom;
 
-     var svg = d3.select(".chart2").append("svg")
-       .attr("width",  outerWidth)
-       .attr("height", outerHeight);
-     var g = svg.append("g")
-       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-     var xAxisG = g.append("g")
-       .attr("class", "x axis")
-       .attr("transform", "translate(0," + innerHeight + ")");
-     var yAxisG = g.append("g")
-       .attr("class", "y axis");
+var svg = d3.select(".finChart").append("svg")
+  .attr("width",  outerWidth)
+  .attr("height", outerHeight);
+var g = svg.append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var xAxisG = g.append("g")
+  .attr("class", "x axis")
+  .attr("transform", "translate(0," + innerHeight + ")");
+var yAxisG = g.append("g")
+  .attr("class", "y axis");
 
-     var xScale = d3.scale.linear().range(      [0, innerWidth]);
-     var yScale = d3.scale.ordinal().rangeBands([0, innerHeight], barPadding);
+var xScale = d3.scale.linear().range(      [0, innerWidth]);
+var yScale = d3.scale.ordinal().rangeBands([0, innerHeight], barPadding);
 
-     var xAxis = d3.svg.axis().scale(xScale).orient("bottom")
-       .ticks(5)                   // Use approximately 5 ticks marks.
-       .tickFormat(d3.format("s")) // Use intelligent abbreviations, e.g. 5M for 5 Million
-       .outerTickSize(0);          // Turn off the marks at the end of the axis.
-     var yAxis = d3.svg.axis().scale(yScale).orient("left")
-       .outerTickSize(0);          // Turn off the marks at the end of the axis.
+var xAxis = d3.svg.axis().scale(xScale).orient("bottom")
+  .ticks(5)                   // Use approximately 5 ticks marks.
+  .tickFormat(d3.format("s")) // Use intelligent abbreviations, e.g. 5M for 5 Million
+  .outerTickSize(0);          // Turn off the marks at the end of the axis.
+var yAxis = d3.svg.axis().scale(yScale).orient("left")
+  .outerTickSize(0);          // Turn off the marks at the end of the axis.
 
-     function render(data){
+function render(data){
 
-       xScale.domain([0, d3.max(data, function (d){ return d[xColumn]; })]);
-       yScale.domain(       data.map( function (d){ return d[yColumn]; }));
+  xScale.domain([0, d3.max(data, function (d){ return d[xColumn]; })]);
+  yScale.domain(       data.map( function (d){ return d[yColumn]; }));
 
-       xAxisG.call(xAxis);
-       yAxisG.call(yAxis);
+  xAxisG.call(xAxis);
+  yAxisG.call(yAxis);
 
-       var bars = g.selectAll("rect").data(data);
-       bars.enter().append("rect")
-         .attr("height", yScale.rangeBand());
-       bars
-         .attr("x", 0)
-         .attr("y",     function (d){ return yScale(d[yColumn]); })
-         .attr("width", function (d){ return xScale(d[xColumn]); });
-       bars.exit().remove();
-     }
+  var bars = g.selectAll("rect").data(data);
+  bars.enter().append("rect")
+    .attr("height", yScale.rangeBand());
+  bars
+    .attr("x", 0)
+    .attr("y",     function (d){ return yScale(d[yColumn]); })
+    .attr("width", function (d){ return xScale(d[xColumn]); });
+  bars.exit().remove();
+}
 
-     function type(d){
-       d.population = +d.population;
-       return d;
-     }
+function type(d){
+  d.population = +d.population;
+  return d;
+}
 
-     render(data)
+render(data)
 }
 //  ====================================================================
 Goals.$inject = ['StrFactory']
